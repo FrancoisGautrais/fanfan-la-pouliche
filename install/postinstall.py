4351a64fb5e10accb8cbb8c753463831827e8849
fanch <francois_gautrais@msn.com>
2022-02-11 23:56:18 +0100

    Ajout de formulaires

--- install/postinstall.py
+++ install/postinstall.py
@@ -3,3 +3,7 @@ from pathlib import Path
 from install import common
 
 os.system(f"bash update_jsx one_shot")
+
+os.chdir("src/fflp")
+os.system("python manage.py migrate")
+os.system("python manage.py runserver")
